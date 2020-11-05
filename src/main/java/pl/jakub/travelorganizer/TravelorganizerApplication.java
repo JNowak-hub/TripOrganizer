bcce2a6db16c3b198a5c1e46c5cbb7d68517b65f
Jakub Nowak <nowakjakub095@gmail.com>
2020-11-05 12:04:06 +0100

    init commit

---
+++ src/main/java/pl/jakub/travelorganizer/TravelorganizerApplication.java
@@ -0,0 +1,13 @@
+package pl.jakub.travelorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelorganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jakub/travelorganizer/TravelorganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakub.travelorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelorganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
